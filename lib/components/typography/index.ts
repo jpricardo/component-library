7588c3eed3bdfd4880cadf8686905c95b76d688b
jpricardo <joaopds.ricardo@gmail.com>
2024-07-24 22:11:49 -0300

    feat: typography components

---
+++ lib/components/typography/index.ts
@@ -0,0 +1,13 @@
+import Body from './Body';
+import Footnote from './Footnote';
+import Headline from './Headline';
+import Label from './Label';
+import Title from './Title';
+
+export class Typography {
+	static Body = Body;
+	static Footnote = Footnote;
+	static Headline = Headline;
+	static Label = Label;
+	static Title = Title;
+}
