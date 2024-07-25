d42970e15125a6e766e1dc256d3c3bda30a7571a
jpricardo <joaopds.ricardo@gmail.com>
2024-07-24 23:22:29 -0300

    chore: storybook config

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,23 @@
-import react from "@vitejs/plugin-react";
-import path from "path";
-import { defineConfig } from "vite";
-import dts from "vite-plugin-dts";
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    lib: {
-      entry: path.resolve(__dirname, "lib/main.ts"),
-      name: "component-library",
-    },
-    copyPublicDir: false,
-    rollupOptions: { external: ["react", "react/jsx-runtime"] },
-  },
+	build: {
+		lib: {
+			entry: path.resolve(__dirname, 'lib/main.ts'),
+			name: 'component-library',
+		},
+		copyPublicDir: false,
+		rollupOptions: { external: ['react', 'react/jsx-runtime'] },
+	},
 
-  plugins: [
-    react(),
-    dts({
-      tsconfigPath: path.resolve(__dirname, "tsconfig.build.json"),
-      rollupTypes: true,
-    }),
-  ],
+	plugins: [
+		react(),
+		dts({
+			tsconfigPath: path.resolve(__dirname, 'tsconfig.build.json'),
+		}),
+	],
 });
