83a3094ac72884807ad53fd060df0fbc60a8ff27
jpricardo <joaopds.ricardo@gmail.com>
2024-07-24 21:44:34 -0300

    chore: vite library mode config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "lib/main.ts"),
+      name: "component-library",
+    },
+    copyPublicDir: false,
+    rollupOptions: { external: ["react", "react/jsx-runtime"] },
+  },
+
+  plugins: [
+    react(),
+    dts({
+      tsconfigPath: path.resolve(__dirname, "tsconfig.build.json"),
+      rollupTypes: true,
+    }),
+  ],
+});
