3bd35b2441572e01eeaec2f122f24f2bf5c90f28
jpricardo <joaopds.ricardo@gmail.com>
2024-07-27 23:27:41 -0300

    fix: trying to fix the styled components bug

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig({
 		lib: {
 			entry: path.resolve(__dirname, 'lib/main.ts'),
 			name: 'component-library',
+			formats: ['es', 'umd'],
 		},
 		copyPublicDir: false,
 		rollupOptions: {
