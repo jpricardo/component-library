e926996ec5d473ddbc29646b3015a14c02dd6e6d
jpricardo <joaopds.ricardo@gmail.com>
2024-07-29 23:38:32 -0300

    fix: text and bg color adjusted

--- lib/components/inputs/Select.tsx
+++ lib/components/inputs/Select.tsx
@@ -1,14 +1,26 @@
 import styled from 'styled-components';
 
 const StyledSelect = styled.select`
+	transition: all 0.2s cubic-bezier(0.645, 0.045, 0.355, 1);
+
 	font-family: ${({ theme }) => theme.typography.fontFamily};
+
+	color: ${({ theme }) => theme.colors.onContainer};
+	background-color: ${({ theme }) => theme.colors.containerLowest};
 
 	font-size: 14px;
 	line-height: 14px;
 	padding: 8px;
 
 	border-radius: 0.125rem;
-	border: 1px solid #c3c3c3;
+	border: 1px solid ${({ theme }) => theme.colors.outline};
+
+	cursor: ${({ disabled }) => (disabled ? 'not-allowed' : 'initial')};
+
+	&::placeholder {
+		color: inherit;
+		opacity: 0.25;
+	}
 `;
 
 type ValueType = string | number;
