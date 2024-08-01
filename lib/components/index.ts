c0fc710beefc3f78ecd44c3101acbf2b1ee61baf
jpricardo <joaopds.ricardo@gmail.com>
2024-07-31 21:48:10 -0300

    fix: added missing export

--- lib/components/index.ts
+++ lib/components/index.ts
@@ -2,6 +2,7 @@ export * from './containers';
 export * from './dataDisplay';
 export * from './feedback';
 export * from './inputs';
+export * from './layout';
 export * from './typography';
 
 export type HtmlAttributes<TElement extends HTMLElement> = React.DetailedHTMLProps<
