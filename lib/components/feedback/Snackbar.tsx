25f84bd00240fd9dce3066b4fb01fe7e1b911e60
jpricardo <joaopds.ricardo@gmail.com>
2025-05-20 22:55:05 -0300

    feat: Snackbar

--- lib/components/feedback/Snackbar.tsx
+++ lib/components/feedback/Snackbar.tsx
@@ -1,48 +1,8 @@
 import { useCallback, useEffect, useState } from 'react';
-import styled, { keyframes } from 'styled-components';
 
+import { Container } from '../containers';
 import { Flex } from '../layout';
 import { Typography } from '../typography';
-
-const StyledCloseBtn = styled.span`
-	cursor: pointer;
-	color: inherit;
-	margin-top: -1px;
-	font-weight: 600;
-	line-height: 10px;
-	user-select: none;
-`;
-
-const fadeIn = keyframes`
-  from {
-    opacity: 0;
-  }
-
-  to {
-    opacity: 1;
-  }
-`;
-
-const StyledSnackbar = styled(Flex)<{ $open: boolean }>`
-	transition: all 0.2s;
-	animation: ${fadeIn} 0.1s linear;
-	display: ${({ $open }) => ($open ? 'flex' : 'none')};
-	cursor: ${({ onClick }) => (onClick ? 'pointer' : 'initial')};
-	user-select: none;
-
-	font-family: ${({ theme }) => theme.typography.fontFamily};
-
-	padding: 0.75rem 1rem;
-	border-radius: 0.125rem;
-
-	box-shadow: ${({ theme }) => theme.shadows.xs};
-	border: 1px solid ${({ theme }) => theme.colors.outline};
-	background-color: ${({ theme }) => theme.colors.container};
-
-	&:hover {
-		background-color: ${({ theme }) => theme.colors.containerHigh};
-	}
-`;
 
 export type SnackbarProps = {
 	id?: string;
@@ -56,7 +16,7 @@ export type SnackbarProps = {
 	onClick?: () => void;
 };
 
-export function Snackbar({ duration = 5000, closable = true, content, ...props }: SnackbarProps) {
+export function Snackbar({ className = '', duration = 5000, closable = true, content, ...props }: SnackbarProps) {
 	const [isOpen, setIsOpen] = useState(true);
 
 	const doClose = useCallback(() => setIsOpen(false), []);
@@ -69,17 +29,22 @@ export function Snackbar({ duration = 5000, closable = true, content, ...props }
 		return () => clearTimeout(timeout);
 	}, [duration, doClose]);
 
-	return (
-		<StyledSnackbar $open={isOpen} justify='space-between' align='center' gap='1rem' {...props}>
-			<Typography.Footnote>{content}</Typography.Footnote>
+	const display = isOpen ? 'flex' : 'hidden';
+	const cursor = props.onClick ? 'cursor-pointer' : 'cursor-initial';
 
-			{closable && (
-				<Typography.Footnote>
-					<StyledCloseBtn onClick={doClose} role='button'>
-						X
-					</StyledCloseBtn>
-				</Typography.Footnote>
-			)}
-		</StyledSnackbar>
+	return (
+		<Container className={`${display} ${cursor} animate-fadein py-2 px-3 ${className}`}>
+			<Flex className='w-full justify-between items-center gap-4' {...props}>
+				<Typography.Footnote>{content}</Typography.Footnote>
+
+				{closable && (
+					<Typography.Footnote>
+						<span className='cursor-pointer font-semibold p-1' onClick={doClose} role='button'>
+							X
+						</span>
+					</Typography.Footnote>
+				)}
+			</Flex>
+		</Container>
 	);
 }
