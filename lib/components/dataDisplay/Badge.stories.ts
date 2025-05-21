dab44b727837ce92c098ba4542d548de9e42c3b0
jpricardo <joaopds.ricardo@gmail.com>
2025-05-21 12:51:07 -0300

    feat: badge

--- lib/components/dataDisplay/Badge.stories.ts
+++ lib/components/dataDisplay/Badge.stories.ts
@@ -9,7 +9,6 @@ const meta: Meta<typeof Badge> = {
 	parameters: { layout: 'centered' },
 	tags: ['autodocs'],
 	args: {
-		variant: 'default',
 		closable: false,
 		children: 'Badge',
 		onClose: fn(),
@@ -30,31 +29,4 @@ export const Closable: Story = {
 		await userEvent.click(canvas.getByRole('button'));
 		expect(args.onClose).toHaveBeenCalled();
 	},
-};
-
-export const Primary: Story = {
-	args: { variant: 'primary' },
-};
-
-export const PrimaryClosable: Story = {
-	args: { variant: 'primary', closable: true },
-	play: Closable.play,
-};
-
-export const Secondary: Story = {
-	args: { variant: 'secondary' },
-};
-
-export const SecondaryClosable: Story = {
-	args: { variant: 'secondary', closable: true },
-	play: Closable.play,
-};
-
-export const Danger: Story = {
-	args: { variant: 'danger' },
-};
-
-export const DangerClosable: Story = {
-	args: { variant: 'danger', closable: true },
-	play: Closable.play,
 };
--- lib/components/dataDisplay/Badge.tsx
+++ lib/components/dataDisplay/Badge.tsx
@@ -1,71 +1,27 @@
-import styled from 'styled-components';
-
 import { HtmlAttributes } from '..';
 import { Flex } from '../layout';
 
-type VariantType = 'default' | 'primary' | 'secondary' | 'danger';
-
-type StyledBadgeProps = {
-	$variant: BadgeProps['variant'];
-};
-
-const StyledBadge = styled.div<StyledBadgeProps>`
-	font-family: ${({ theme }) => theme.typography.fontFamily};
-	font-size: 0.75rem;
-	line-height: 0.75rem;
-
-	padding: 0.25rem 0.5rem;
-	border-radius: 0.25rem;
-	cursor: pointer;
-
-	color: ${({ $variant, theme }) => {
-		if ($variant === 'primary') return theme.colors.primary;
-		if ($variant === 'secondary') return theme.colors.secondary;
-		if ($variant === 'danger') return theme.colors.error;
-		return theme.colors.onSurface;
-	}};
-	background-color: ${({ $variant, theme }) => {
-		if ($variant === 'primary') return theme.colors.primaryContainer;
-		if ($variant === 'secondary') return theme.colors.secondaryContainer;
-		if ($variant === 'danger') return theme.colors.errorContainer;
-		return theme.colors.surface;
-	}};
-
-	border-width: 1px;
-	border-style: solid;
-	border-color: ${({ $variant, theme }) => {
-		if ($variant === 'primary') return theme.colors.primary;
-		if ($variant === 'secondary') return theme.colors.secondary;
-		if ($variant === 'danger') return theme.colors.error;
-		return theme.colors.outline;
-	}};
-`;
-
-const StyledCloseBtn = styled.span`
-	color: inherit;
-	margin-top: -1px;
-	font-weight: 600;
-	line-height: 10px;
-	user-select: none;
-`;
-
 export type BadgeProps = HtmlAttributes<HTMLDivElement> & {
-	variant?: VariantType;
 	closable?: boolean;
 	onClose?: () => void;
 };
-export function Badge({ variant = 'default', closable, children, onClose, ...props }: BadgeProps) {
+export function Badge({ className = '', closable, children, onClose, ...props }: BadgeProps) {
 	return (
-		<StyledBadge $variant={variant} {...props}>
-			<Flex align='center' gap='0.5rem'>
+		<div
+			className={`font-sans text-xs cursor-pointer py-0.5 px-2 text-neutral-950 dark:text-neutral-100 bg-neutral-100 dark:bg-neutral-900 rounded-sm border border-solid border-neutral-300 dark:border-neutral-700 ${className}`}
+			{...props}
+		>
+			<Flex style={{ alignItems: 'center', gap: '.5rem' }}>
 				{children}
 
 				{closable && (
-					<StyledCloseBtn onClick={onClose} role='button'>
+					<span className='cursor-pointer select-none font-semibold p-0' onClick={onClose} role='button'>
 						x
-					</StyledCloseBtn>
+					</span>
 				)}
 			</Flex>
-		</StyledBadge>
+		</div>
 	);
 }
+
+// border-neutral-300 dark:border-neutral-700
