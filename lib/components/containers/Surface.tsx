071d00ee081671024b953380774638874d649c32
jpricardo <joaopds.ricardo@gmail.com>
2025-05-21 12:36:37 -0300

    fix: Surface bg color set to white

--- lib/components/containers/Surface.tsx
+++ lib/components/containers/Surface.tsx
@@ -5,7 +5,7 @@ export type SurfaceProps = HtmlAttributes<HTMLDivElement>;
 export function Surface({ className = '', ...props }: SurfaceProps) {
 	return (
 		<div
-			className={`font-sans p-8 rounded-sm text-neutral-950 dark:text-neutral-100 bg-neutral-10 dark:bg-neutral-800 ${className}`}
+			className={`font-sans p-8 rounded-sm text-neutral-950 dark:text-neutral-100 bg-white dark:bg-neutral-800 ${className}`}
 			{...props}
 		/>
 	);
