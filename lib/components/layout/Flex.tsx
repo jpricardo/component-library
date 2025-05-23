520f1102925dc0fee15659ac9fd72d14952e4e4b
jpricardo <joaopds.ricardo@gmail.com>
2025-05-22 21:06:40 -0300

    chore: Flex deprecated

--- lib/components/layout/Flex.tsx
+++ lib/components/layout/Flex.tsx
@@ -4,6 +4,9 @@ export type FlexProps = HtmlAttributes<HTMLDivElement> & {
 	vertical?: boolean;
 };
 
+/**
+ * @deprecated Redundant, just not that useful. `<div className='flex' />`  is enough.
+ */
 export function Flex({ className = '', vertical, ...props }: FlexProps) {
 	const orientation = vertical ? 'flex-col' : 'flex-row';
 
