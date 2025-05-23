f60c5699f03ea9890efecf9c2f646982f2b13dca
jpricardo <joaopds.ricardo@gmail.com>
2025-05-22 21:03:45 -0300

    fix: removed rounded borders

--- lib/components/containers/Surface.tsx
+++ lib/components/containers/Surface.tsx
@@ -5,7 +5,7 @@ export type SurfaceProps = HtmlAttributes<HTMLDivElement>;
 export function Surface({ className = '', ...props }: SurfaceProps) {
 	return (
 		<div
-			className={`rounded-sm bg-white p-8 font-sans text-neutral-950 dark:bg-neutral-800 dark:text-neutral-100 ${className}`}
+			className={`rounded-none bg-white p-8 font-sans text-neutral-950 dark:bg-neutral-800 dark:text-neutral-100 ${className}`}
 			{...props}
 		/>
 	);
